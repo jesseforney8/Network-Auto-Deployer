8d60a504cdb4c178b3cbadf6ba06bacd9fe416b2
Jesse <jesseforney8@gmail.com>
2024-10-08 16:19:44 -0500

    Cteaed basic GUI for config tab and added configs

--- NAD/main.py
+++ NAD/main.py
@@ -43,10 +43,10 @@ def upload_config():
 
 
 
-
+#creates tabs
 notebook = ttk.Notebook(root)
 
-tab1 = tk.Frame(notebook)
+tab1 = tk.Frame(notebook, padx=20)
 tab2 = tk.Frame(notebook)
 tab3 = tk.Frame(notebook)
 
@@ -57,14 +57,26 @@ notebook.add(tab3, text='Push')
 notebook.grid()
 
 
+#tab1
+add_new_device_label = tk.Label(tab1, text="Upload Configuration File (.txt)", pady=10).grid(row=1, column=0)
+upload_btn = tk.Button(tab1, text="Upload", command=upload_config).grid(row=2, column=0)
+delete_config_btn = tk.Button(tab1, text="Delete").grid(row=3, column=0)
 
-add_new_device_label = tk.Label(tab1, text="Upload Config")
-add_new_device_label.grid(row=0, column=0)
+my_ctree = ttk.Treeview(tab1)
 
+my_ctree["columns"] = ("name", "date_uploaded")
 
-upload_btn = tk.Button(tab1, text="Upload", command=upload_config)
-upload_btn.grid(row=1, column=0)
-    
+my_ctree.column("#0", width=0, stretch="no")
+my_ctree.column("name", width=120, minwidth=25, anchor="w")
+my_ctree.column("date_uploaded", width=120, minwidth=25, anchor="w")
+
+my_ctree.heading("#0", text="", anchor="w")
+my_ctree.heading("name", text="Config Name", anchor="w")
+my_ctree.heading("date_uploaded", text="Date Uploaded", anchor="w")
+
+my_ctree.grid(row=0, column=0)
+
+#tab2
 
 #Treeview for devices
 
